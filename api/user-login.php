e33d3cad93dd7765fdd69c40275ed1c740d8f610
Fábio Santos <fabio3268@gmail.com>
2022-12-02 07:39:17 -0300

    Acessando Banco de Dados - Register - Login

--- api/user-login.php
+++ api/user-login.php
@@ -1,3 +1,34 @@
 <?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "bd-clinica";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+];
 
-var_dump($_POST);
+$conn = new PDO(
+    "mysql:host=$host;dbname=$database",
+    $user,
+    $password,
+    $options
+);
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query = "SELECT * 
+          FROM users 
+          WHERE email LIKE '$email' AND 
+                password LIKE '$password'";
+
+$stmt = $conn->query($query);
+
+if($stmt->rowCount() == 1){
+    $row = $stmt->fetch();
+    echo "Olá, {$row->name}!";
+} else{
+    echo "Email ou Senha não cadastrados";
+}
+
